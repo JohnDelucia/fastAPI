aea6b9fc4c34b43d95c5bdc3872fab9383ba2314
JohnDelucia <johndeluciabrunswick@gmail.com>
2022-09-04 17:35:56 -0400

    correct CORSMiddleware import statement

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 
-from fastapi import FastAPI, CORSMiddleware
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
@@ -8,16 +9,16 @@ from .config import settings
 
 app = FastAPI()
 
-#list of url to talk to API
+# list of url to talk to API
 origins = ["*"]
 
-#allows for requests from other domains to talk with our API
+# allows for requests from other domains to talk with our API
 app.add_moddleware(
     CORSMiddleware,
-    allow_origins=origins, 
-    allow_credentials=True, 
-    allow_methods = ["*"],
-    allow_headers = ["*"]
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(post.router)  # request go es to post and looks for match
