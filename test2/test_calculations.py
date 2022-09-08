b9a9e4947852feb89c5773a6c418c8cb51aa015a
JohnDelucia <johndeluciabrunswick@gmail.com>
2022-09-07 22:49:27 -0400

    testing

--- test2/test_calculations.py
+++ test2/test_calculations.py
@@ -1,6 +1,13 @@
+import pytest
+
 from test2.calculations import add
 
-
-def test_add():
-    sum = add(5, 10)
-    assert sum == 15
+@pytest.mark.parametrize("num1, num2, expected", [
+    (3, 2, 5),
+    (4, 5, 9),
+    (12, 4, 16)
+])
+def test_add(num1, num2, expected):
+    sum = add(num1, num2)
+    assert sum == expected
+ 
